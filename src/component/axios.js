91b1274278a4595bceaf47916cff83f2cafd1342
Saleamlak21 <fentaneshmesfin@gmail.com>
2023-11-08 11:42:09 -0500

    some pakeage modification

--- src/component/axios.js
+++ src/component/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:(`${process.env.EVANGADI_FORUM_BACKEND_BASE_URL}`), // Use http instead of https, and include the correct port number
+    baseURL:process.env.EVANGADI_FORUM_BACKEND_BASE_URL // Use http instead of https, and include the correct port number
 })
 
 export default instance;
